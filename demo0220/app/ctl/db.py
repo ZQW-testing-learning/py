5aff60ff9ab46aa12a66aecdb90d798bd49b228f
quanwei <quanwei_275@126.com>
2025-03-06 03:47:18 +0800

    test

--- demo0220/app/ctl/db.py
+++ demo0220/app/ctl/db.py
@@ -1,29 +1,48 @@
 # from app.db import get_db
 
 class Mode:
-    def __init__():
-        print('Mode init')
+    def __init__(self, **kwargs):
+        self.table_name = self.__class__.get_class_name().lower()
+        self.properties = kwargs
+        print('Mode init', self.properties)
 
-def create_database(tname: str, properties: dict):
-    pps = []
-    for k, v in properties.items():
-        pps.append(f"{k} {v}")
-    PPS = ", ".join(pps)
-        
-    return f'''CREATE TABLE IF NOT EXISTS {tname} ({PPS})'''
+    def __iter__(self):
+        for k, v in self.properties.items():
+            yield k, v
 
-def insert(tname: str, **opts: dict):
-    fk = ', '.join([k for k in opts.keys()])
-    fv = ', '.join([f"'{v}'" if isinstance(v, str) else str(v) for v in opts.values() ])
-    return f'''INSERT INTO {tname} ({fk}) VALUES ({fv})'''
+    def create_database(self, properties: dict):
+        pps = []
+        for k, v in properties.items():
+            pps.append(f"{k} {v}")
+        PPS = ", ".join(pps)
+            
+        return f'''CREATE TABLE IF NOT EXISTS {self.table_name} ({PPS})'''
 
-def update(tname: str, **opts: dict):
-    where = opts.get('where', None)
-    ws = []
-    # if where is not None:
-        
-    return f'''UPDATE {tname} SET WHERE '''
+    def insert(self, **opts: dict):
+        fk = ', '.join([k for k in opts.keys()])
+        fv = ', '.join([f"'{v}'" if isinstance(v, str) else str(v) for v in opts.values() ])
+        return f'''INSERT INTO {self.table_name} ({fk}) VALUES ({fv})'''
+
+    def update(tname: str, **opts: dict):
+        where = opts.get('where', None)
+        ws = []
+        # if where is not None:
+            
+        return f'''UPDATE {tname} SET WHERE '''
+
+    @classmethod
+    def get_class_name(cls):
+        return cls.__name__
+
+
+
+class Users(Mode):
+    name: str = ''
+    age: int = 0
+    phone: str = ''
+
+
 
 if __name__ == "__main__":
-    # print(create_database('users', {"id": "INTEGER PRIMARY KEY AUTOINCREMENT", "name": "TEXT", "age": "INTEGER"}))
-    print(insert("users", name='zqw', age=25))
+    u = Users(name="zqw", age=25, phone="1234567890")
+    print(u.insert(**dict(u)))
